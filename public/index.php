a7ec252e0ae5501bf1913cbfbfdd26b5c92f6341
Richard Walker <monkeybruiser@richards-mbp.home>
2017-09-20 22:35:59 +0100

    Starting up base template

---
+++ public/index.php
@@ -0,0 +1,98 @@
+<?php
+
+    include('modules/layout/header.php' );
+
+?>
+
+<section class="">
+
+    <div class="wrap">
+
+        <h1>Have we got the tubs for you</h1>
+
+        <div class="">
+            <img alt="#" src="http://placehold.it/300x200" />
+        </div>
+
+    </div>
+
+</section>
+
+<section class="">
+
+    <div class="wrap">
+
+        <div class="grid">
+
+            <?php
+
+                $i		= 1;
+                $max	= 5;
+
+                while($i <= $max) :
+
+                    $product_image              = true;
+                    if($product_image) {
+                        $product_image_alt      = '#';
+                        $product_image_url      = 'http://placehold.it/800x500';
+                    }
+                    $product_name               = 'Tub name here';
+                    $product_desc               = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, nibh et porttitor consectetur, massa ligula sodales nulla, vel faucibus eros diam eu nisi. Vestibulum nec sapien vel leo vestibulum lobortis. Nam dui neque, lobortis non sagittis nec, venenatis at libero. Sed at libero libero.</p>';
+                    $product_url                = '#';
+
+            ?>
+            <div class="grid__col grid__col--1-of-2 grid__col--m-1-of-1 grid__col--l-1-of-1">
+
+                <div class="">
+
+                    <?php
+
+                        if($product_image) {
+
+                    ?>
+                    <div class="">
+                        <img alt="<?php echo $product_image_alt; ?>" src="<?php echo $product_image_url; ?>" />
+                    </div>
+                    <?php
+
+                        }
+
+                    ?>
+                    <h2 class=""><?php echo $product_name; ?></h2>
+                    <?php
+
+                        if($product_desc) {
+
+                    ?>
+                    <div class="">
+                        <?php echo $product_desc; ?>
+                    </div>
+                    <?php
+
+                        }
+
+                    ?>
+                    <p class="button-wrap"><a href="<?php echo $product_url; ?>" title="View more about <?php echo $product_name; ?>">View</a></p>
+
+                </div>
+
+            </div>
+            <?php
+
+                    $i++;
+
+                endwhile;
+
+            ?>
+
+        </div>
+
+    </div>
+
+</section>
+
+<?php
+
+    include('modules/layout/footer.php' );
+
+?>
---
+++ public/modules/layout/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Ton's Tubs</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="library/css/style-local.css">
+        <script src="library/js/libs/modernizr.js"></script>
+    </head>
+    <body>
+
+        <div class="site-wrap">
+
+            <section class="mast">
+
+                <div class="wrap">
+
+                    <div class="logo">
+                        <img alt="#" class="" src="http://placehold.it/70x70" />
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li><a href="#" title="#">Home</a></li>
+                            <li><a href="#" title="#">One</a></li>
+                            <li><a href="#" title="#">Two</a></li>
+                            <li><a href="#" title="#">Three</a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+
+            </section>
